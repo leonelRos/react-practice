4cff79537eb4a8a0a6ef19f170ed46365da3fae4
Leonel Rosales <rleonel426@gmail.com>
2020-04-06 22:49:45 -0700

    adding functionality

--- src/Component/counter.jsx
+++ src/Component/counter.jsx
@@ -2,19 +2,26 @@ import React, {Component} from "react";
 
 class Counter extends Component {
     state = {
-        count : 3
+        count : 0
     };
+    getIncrement = () => {
+        console.log("this is incrementing", this)
+    }
     render (){
         
         return (
             <div>
                 {/* // we can write any javascript syntax in the curly braces */}
-                <span className={this.getBadgeColor()}>{this.createFormat()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                <span className={this.getBadgeColorClasses()}>{this.createFormat()}</span>
+                <button onClick={this.getIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                {/* <ul>
+                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+                </ul> */}
             </div>
         )
     }
-    getBadgeColor() { //is the method to change color once is different than zero
+
+    getBadgeColorClasses() { //is the method to change color og className once is different than zero
         let classes = "badge m-2 badge-";
         classes += (this.state.count === 0) ? "warning" : "primary";
         return classes;
