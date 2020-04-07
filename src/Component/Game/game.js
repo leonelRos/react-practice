62f78b894e0eb7fa2dd896cecc9f3cddec8258c1
Leonel Rosales <rleonel426@gmail.com>
2020-04-06 20:28:38 -0700

    first commit

---
+++ src/Component/Game/game.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import '../Game/game.css'
+
+// const CELL_SIZE= 71;
+// const WIDTH = 600;
+// const HEIGHT = 600;
+
+class Game extends  Component{
+    render(){
+        return(
+            <div>
+                <h1> this i a home
+
+                </h1>
+            </div>
+        )
+    }
+}
+export default Game;
