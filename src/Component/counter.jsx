47b60cbadd19e8234240744b1190912664acb4e9
Leonel Rosales <rleonel426@gmail.com>
2020-04-07 16:02:55 -0700

    adding functionality

--- src/Component/counter.jsx
+++ src/Component/counter.jsx
@@ -1,21 +1,21 @@
 import React, {Component} from "react";
 
 class Counter extends Component {
-    state = {
-        value : this.props.counter.value
-    };
+    // state = {
+    //     value : this.props.counter.value
+    // };
     //by using arrow function i sae myself from using constructor. THIS is a relation of the object 
-    getIncrement = () => {       
-        //this increments the state by 1
-        this.setState({ value: this.state.value + 1}) 
-    }
+    // getIncrement = () => {       
+    //     //this increments the state by 1
+    //     this.setState({ value: this.state.value + 1}) 
+    // }
     render (){
         
         return (
             <div>
                 {/* // we can write any javascript syntax in the curly braces */}
                 <span className={this.getBadgeColorClasses()}>{this.createFormat()}</span>
-                <button onClick={this.getIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
                 {/* <ul>
                     {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
                 </ul> */}
@@ -26,13 +26,13 @@ class Counter extends Component {
 
     getBadgeColorClasses() { //is the method to change color og className once is different than zero
         let classes = "badge m-2 badge-";
-        classes += this.state.value === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     createFormat(){
         // we can ssign a variable in curly brackets instead of this.state.count
-        const {value} = this.state
+        const {value} = this.props.counter;
         //this statemtn says if count is equal to 0 return the word ZERO otherwise return count
         return value === 0 ? "zero" : value;
     }
--- src/Component/counters.jsx
+++ src/Component/counters.jsx
@@ -10,6 +10,24 @@ class Counters extends Component {
             {id: 4, value: 0}
         ]
     }
+    handleIncrement = counter => {
+       const counters = [...this.state.counters];
+       const index = counters.indexOf(counter)
+       counters[index] = {...counter}
+       counters[index].value++;
+       console.log(this.state.counters[index]) 
+       this.setState({counters});
+    }   
+
+    handleReset = () => {
+        // we are reseting the structure but not the DOM
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        })
+        this.setState({counters})
+    }
+
     handleDecrease = counterId => {
         const counters = this.state.counters.filter(c => c.id !== counterId);
         this.setState({counters});
@@ -18,10 +36,12 @@ class Counters extends Component {
     render() { 
         return ( 
             <div>
+                <button onClick={this.handleReset}className="btn btn-primary btn-sm m-2">Reset</button>
                 {this.state.counters.map(counter => (
                 <Counter 
                 key={counter.id} 
                 onDecrease={this.handleDecrease} 
+                onIncrement={this.handleIncrement}
                 counter={counter} 
                 />))}
             </div>
