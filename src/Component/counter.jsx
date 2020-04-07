af3bd2155a562ac27435104ff25c4911514be953
Leonel Rosales <rleonel426@gmail.com>
2020-04-07 00:30:17 -0700

    adding functionality

--- src/Component/counter.jsx
+++ src/Component/counter.jsx
@@ -26,15 +26,15 @@ class Counter extends Component {
 
     getBadgeColorClasses() { //is the method to change color og className once is different than zero
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     createFormat(){
         // we can ssign a variable in curly brackets instead of this.state.count
-        const {count} = this.state
+        const {value} = this.state
         //this statemtn says if count is equal to 0 return the word ZERO otherwise return count
-        return count === 0 ? "zero" : count;
+        return value === 0 ? "zero" : value;
     }
 }
  
